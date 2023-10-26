37658a25c73c5024400107f41a8762818fa25e10
KrishRamani2 <krishramani02@gmail.com>
2023-10-26 23:09:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import './index.css'
+const App = () =>{
+  const [name,setname] = useState();
+  const [fullName,setFullName]=useState();
+  const [pass,setpass] = useState();
+  const [fullpass,setfullpass] = useState();
+  const [mob,setmob] = useState();
+  const [fullmob,setFullMob]=useState();
+  const [Email,setemail] = useState();
+  const [fullemail,setfullemail] = useState();
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const inputEvent=(event)=>{
+    setname(event.target.value);
+  };
+  const passEvent=(event)=>{
+    setpass(event.target.value);
+  };
+  const emailEvent=(event)=>{
+    setemail(event.target.value);
+  };
+  const mobEvent=(event)=>{
+    setmob(event.target.value);
+  };
+  const onSubmit=(event)=>{
+    event.preventDefault();
+    setFullName(name);
+    setfullpass(pass);
+    setfullemail(Email);
+    setFullMob(mob);
+    alert("Form Submitted");
+  };
+  return(<>
+  <div className="main_div">
+  <form onSubmits={onSubmit}>
+  <div>
+    <h1>Hello {fullName} {fullpass}</h1>
+    <p> {fullemail}</p>
+    <p>{fullmob}</p>
+    <input type='text' placeholder="Enter Your Name" 
+    onChange={inputEvent}    
+    value={name}
+    />
+    <br />
+    <input type='password' placeholder="Enter Your Password"
+    onChange={passEvent}
+    value={pass} 
+    />
+
+<br />
+    <input type='email' placeholder="Enter Your Email Id"
+    onChange={emailEvent}
+    value={Email} 
+    />
+    <br />
+    <input type='phone' placeholder="Enter Your Mobile Number"
+    onChange={mobEvent}
+    value={mob}
+    />
+    <button onClick={onSubmit}>Login</button>
+  </div>
+  </form>
+  </div>
+  </>);
 }
 
 export default App;
